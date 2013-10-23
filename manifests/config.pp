54580b200f69f7628796526fefaab75eceeed9b8
Adam Breaux <breauxaj@gmail.com>
2013-10-23 09:39:42 -0400

    rethink

---
+++ manifests/config.pp
@@ -0,0 +1,39 @@
+define coldfusion::config (
+  $cf_home = '/opt/coldfusion10',
+  $owner = 'deploy',
+  $group = 'deploy'
+) {
+  file { $cf_home:
+    ensure => directory,
+    owner  => $owner,
+    group  => $group,
+    mode   => '0775',
+  }
+
+  file { '/etc/profile.d/coldfusion.sh':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    content => template('coldfusion/profile.erb'),
+  }
+
+  file { '/usr/lib64/httpd/modules/mod_jk22.so':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/coldfusion/mod_jk22.so',
+    require => Package['httpd'],
+  }
+
+  file { '/etc/httpd/conf.d/jk.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/coldfusion/jk.conf',
+    require => Package['httpd'],
+  }
+
+}
