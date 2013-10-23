5c9b75ac7894dac5b9802648de51e3be26ed887a
Adam Breaux <breauxaj@gmail.com>
2013-10-23 09:38:57 -0400

    outside of tree so it's safe

---
+++ manifests/workers.pp
@@ -0,0 +1,21 @@
+define coldfusion::workers (
+  $workers = ''
+) {
+  file { '/etc/httpd/jk.d':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package['httpd'],
+  }
+
+  file { "/etc/httpd/jk.d/workers.properties":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('coldfusion/workers.erb'),
+    require => File['/etc/httpd/jk.d'],
+  }
+
+}
