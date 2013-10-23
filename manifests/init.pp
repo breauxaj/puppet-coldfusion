d2ac534bb7936580ace816e70aa58d926610e70f
Adam Breaux <breauxaj@gmail.com>
2013-10-23 08:39:36 -0400

    empty

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,3 @@
 class coldfusion {
-  $paths = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ '/opt/coldfusion10' ],
-  }
-
-  file { $paths:
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0775',
-  }
-
-  file { '/etc/profile.d/coldfusion.sh':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => 'puppet:///modules/coldfusion/coldfusion.profile'
-  }
-
-  file { '/etc/httpd/conf/mod_jk.conf':
-    ensure => absent,
-  }
 
 }
