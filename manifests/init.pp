767d5ea166fe23640c8bbe2980433947bb2e68d5
Adam Breaux <breauxaj@gmail.com>
2014-07-07 19:41:35 -0400

    added doc

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,20 @@
+# Class: coldfusion
+#
+# This class does nothing
+#
+# Parameters:
+#
+# Actions:
+#   - Does nothing
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'coldfusion': }
+#
 class coldfusion {
 
 }
