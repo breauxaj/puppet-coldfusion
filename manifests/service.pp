9464d0e84bef7d865122e1e17db751ea0f55d63d
Adam Breaux <breauxaj@gmail.com>
2014-07-07 20:08:54 -0400

    added docs

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,3 +1,33 @@
+# Define: coldfusion::service
+#
+# This define manages the Coldfusion service
+#
+# Parameters:
+#
+#  name:
+#    Determines the actual name of the service by concatenation
+#
+#  ensure:
+#    Controls the state of the service
+#
+#  enable:
+#    Controls the service start on boot
+#
+# Actions:
+#   - Stops/starts the Coldfusion service
+#   - Stages the init script for use
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  To enable the service, use:
+#
+#    coldfusion::service { 'cfusion':
+#      ensure => running,
+#      enable => true
+#    }
+#
 define coldfusion::service (
   $ensure,
   $enable
