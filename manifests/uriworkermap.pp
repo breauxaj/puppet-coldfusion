d33913b6a32d8cdb0fddb1b2be96227239e025df
Adam Breaux <breauxaj@gmail.com>
2014-07-08 09:16:28 -0400

    add docs

--- manifests/uriworkermap.pp
+++ manifests/uriworkermap.pp
@@ -1,3 +1,23 @@
+# Define: coldfusion::uriworkermap
+#
+# This define creates the uriworkermap file
+#
+# Parameters:
+#
+#  name:
+#    Specify the worker name
+#
+# Actions:
+#   - Creates a uriworkermap file in /etc/httpd/jk.d
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  To create a file, use:
+#
+#      coldfusion::uriworkermap { 'app_cluster': }
+#
 define coldfusion::uriworkermap {
   $jkd = $::operatingsystem ? {
     /(?i-mx:centos|fedora|redhat|scientific)/ => '/etc/httpd/jk.d',
