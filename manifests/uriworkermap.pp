a2621d2e5f29ce6c8ef417144fd2bf3e3121ab87
Adam Breaux <breauxaj@gmail.com>
2014-07-08 08:11:06 -0400

    should be auto-required

--- manifests/uriworkermap.pp
+++ manifests/uriworkermap.pp
@@ -1,11 +1,14 @@
 define coldfusion::uriworkermap {
-  file { "/etc/httpd/jk.d/uri-${name}-map.properties":
+  $jkd = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => '/etc/httpd/jk.d',
+  }
+
+  file { "${jkd}/uri-${name}-map.properties":
     ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => template('coldfusion/uriworkermap.erb'),
-    require => File['/etc/httpd/jk.d'],
   }
 
 }
