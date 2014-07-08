e3f10d96e679a44579ff7fecf113ebace2cc3f4f
Adam Breaux <breauxaj@gmail.com>
2014-07-08 09:06:13 -0400

    added docs

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,35 @@
+# Define: coldfusion::config
+#
+# This define sets environment variables and install location
+#
+# Parameters:
+#
+#  cf_home:
+#    Specify where ColdFusion is installed
+#
+#  owner:
+#    Enforce the owner of the install folder
+#
+#  group:
+#    Enforce the group of the install folder
+#
+# Actions:
+#   - Creates a profile.d script to set environment variable
+#   - Manage owner/group of install folder
+#   - Configure init scripts via sysconfig file
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  To setup install path and hint for init scripts, use:
+#
+#    coldfusion::config { 'default':
+#      cf_home => '/opt/coldfusion10',
+#      owner   => 'deploy',
+#      group   => 'deploy'
+#    }
+#
 define coldfusion::config (
   $cf_home = '/opt/coldfusion10',
   $owner = 'deploy',
